aa00d4b1b05e27235ce937aeaded9850ba75048f
Michael Piechotta <michael.piechotta@gmail.com>
2019-05-21 23:52:32 +0200

    Harmonize jacusa2 list object

--- R/add-bc.R
+++ R/add-bc.R
@@ -1,44 +1,44 @@
-#' Add base call count matrix to JACUSA list object.
+# TODO 
+# * finish this
+# * adjust read_result
+# * adjust test(s)
+
+#' Add base call count matrix to JACUSA2 list object.
 #'
-#' \code{add_bc_matrix()} adds base call count matrix for both 
-#' conditions to JACUSA list object. JACUSA list object must have the following 
-#' fields set: "bases1*" and "bases2*". Each "basesij" field, where i = condition 
-#' and j respective replicate number, encodes base call counts (A,C,G,T) that are 
-#' separated by ",": 10,2,0,0
+#' \code{add_bc_matrix()} adds base call count matrix for arbitrary number 
+#' of conditions to JACUSA2 list object. List object must have the following 
+#' field: "base_str[[i]][[j]]" where i = condition and j respective replicate number, 
+#' encodes base call counts (A,C,G,T) that are separated by ",": 10,2,0,0
 #'
-#' @param jacusa List object created by \code{read_jacusa()}.
-#' @param bc_str1 Vector or list of string vectors that encode base calls for 
-#' condition 1.
-#' @param bc_str2 Vector or list of string vectors that encode base calls for 
-#' condition 2.
+#' @param jacusa2 List object created by \code{read_result()}.
 #' 
-#' @return Returns a JACUSA list object with "bc_matrix1" and "bc_matrix2" fields 
-#' that contain the respective base call count matrix.
+#' @return Returns a JACUSA2 list object with "bc_matrix[[condition]][replicate]]" field 
+#' that contains the respective base call count matrix.
 #' 
 #' @export
-add_bc_matrix <- function(jacusa, 
-                          bc_str1 = get_bc_str4condition(jacusa, 1), 
-                          bc_str2 = get_bc_str4condition(jacusa, 2)) {
-  jacusa$bc_matrix1 <- bc_str2bc_matrix(bc_str1)
-  jacusa$bc_matrix2 <- bc_str2bc_matrix(bc_str2)
-  jacusa
+add_bc_matrix <- function(jacusa2) {
+  # TODO how many conditions
+  conditions <- "TODO"
+  
+  # OLD
+  # jacusa2$bc_matrix1 <- bc_str2bc_matrix(bc_str1)
+  # jacusa2$bc_matrix2 <- bc_str2bc_matrix(bc_str2)
+  # TODO jacusa$bc_matrix[1,2]
+  jacusa2
 }
 
-#' Add string vector of observed base calls for each condition to JACUSA list 
+#' Add string vector of observed base calls for each condition to JACUSA2 list 
 #' object.
 #'
 #' \code{add_bc()} calculates a vector of observed base calls for each condition 
-#' and adds "bc1" and "bc2" fields to the JACUSA list object. 
-#' Make sure \code{add_bc_matrix()} has been called to populate "bc_matrix1" 
-#' and "bc_matrix2" fields or set option "aa" (i.e. auto_add) to TRUE.
+#' and adds "bc[[condition]][replicate]] field to the JACUSA2 list object. 
+#' Make sure \code{add_bc_matrix()} has been called to populate "bc_matrix" or 
+#' set option "aa" (i.e. auto_add) to TRUE.
 #' All necessary but missing fields will be added automatically, possibly 
-#' overriden existing fields of the JACUSA list object.
+#' overriden existing fields.
 #'
-#' @param jacusa List object created by \code{read_jacusa()}.
-#' @param bc_matrix1 Base call matrix of list of base call matrices for 
-#' condition 1.
-#' @param bc_matrix2 Base call matrix of list of base call matrices for 
-#' condition 2.
+#' @param jacusa2 List object created by \code{read_result()}.
+#' @param bc_matrix List of Base call matrix lists.
 #' @param aa Logical indicates if missing fields should added automatically.
 #' 
 #' @return Returns a JACUSA list object with additional Base call (BC) fields: 
@@ -56,22 +56,19 @@ add_bc_matrix <- function(jacusa,
 #' barplot(table(jacusa$bc1))
 #' 
 #' @export 
-add_bc <- function(jacusa, 
-                   bc_matrix1 = jacusa$bc_matrix1, 
-                   bc_matrix2 = jacusa$bc_matrix2,
-                   aa = FALSE) {
+add_bc <- function(jacusa2, bc_matrix = jacusa2$bc_matrix, aa = FALSE) {
   # auto add missing fields
   if (aa) {
-    jacusa <- add_bc_matrix(jacusa)
-    bc_matrix1 = jacusa$bc_matrix1 
-    bc_matrix2 = jacusa$bc_matrix2
+    jacusa2 <- add_bc_matrix(jacusa2)
+    bc_matrix2 = jacusa$bc_matrix 
   }
-  jacusa$bc1 = get_bc(bc_matrix1)
-  jacusa$bc2 = get_bc(bc_matrix2)
-  jacusa
+  # TODO
+  jacusa2$bc = get_bc(bc_matrix)
+  jacusa2
 }
 
-#' Add base call change for RDD comparisons of a JACUSA result file.
+# TODO make call-1 possible, how to deal with RRD?
+#' Add base call change for RDD comparisons of a JACUSA2 result file.
 #'
 #' \code{add_bc_change()} adds base call changes for gDNA vs. cDNA comparisons.
 #' Make sure \code{add_bc()} has been called to populate "bc1" and "bc2" fields or 
@@ -79,42 +76,46 @@ add_bc <- function(jacusa,
 #' All necessary but missing fields will be added automatically, possibly 
 #' overriden existing fields of the JACUSA list object.
 #'
-#' @param jacusa List object created by \code{read_jacusa()}.
-#' @param bc1 Vector of observed base calls for condition 1.
-#' @param bc2 Vector of observed base calls for condition 2.
+#' @param jacusa2 List object created by \code{read_jacusa()}.
+#' @param ref TODO 0 => use ref_base > 0 => condition in bc
+#' @param bc List of Vector of observed base calls.
 #' @param aa Logical indicates if missing fields should added automatically.
 #'
 #' @return Returns a JACUSA object list with the additional "bc_change" field.
 #'
 #' @examples
 #' ## populate necessary fields manually
-#' jacusa <- add_bc_matrix(rdd)
-#' jacusa <- add_bc(jacusa)
-#' jacusa <- add_bc_change(jacusa)
+#' jacusa2 <- add_bc_matrix(rdd)
+#' jacusa2 <- add_bc(jacusa2)
+#' jacusa2 <- add_bc_change(jacusa2)
 #' ## plot distribution of base changes
-#' barplot(table(jacusa$bc_change))
+#' barplot(table(jacusa2$bc_change))
 #'
 #' ## populate necessary fields automatically
-#' jacusa <- add_bc_change(jacusa, aa = TRUE)
+#' jacusa2 <- add_bc_change(jacusa2, ref = 1, aa = TRUE)
 #' ## plot distribution of base changes
-#' barplot(table(jacusa$bc_change))
+#' barplot(table(jacusa2$bc_change))
 #' 
 #' @export 
-add_bc_change <- function(jacusa, 
-                          bc1 = jacusa$bc1,
-                          bc2 = jacusa$bc2,
+add_bc_change <- function(jacusa2, 
+                          ref = 0,
+                          bc = jacusa2$bc,
                           aa = FALSE) {
   # auto add missing fields
   if (aa) {
-    jacusa <- add_bc(jacusa, aa = TRUE)
-    bc1 = jacusa$bc1
-    bc2 = jacusa$bc2
+    jacusa2 <- add_bc(jacusa2, aa = TRUE)
+    bc = jacusa2$bc
   }
-  jacusa[["bc_change"]] <- get_bc_change(bc1, bc2)
-  jacusa
+  if (ref > 0) {
+    dna <- bc[[ref]]
+  } else {
+    dna <- jacusa2$ref_base
+  }
+  jacusa2$bc_change <- get_bc_change(dna, rna)
+  jacusa2
 }
 
-#' Add base call change ratio (e.g.: editing frequency) to a JACUSA object list.
+#' Add base call change ratio (e.g.: editing frequency) to a JACUSA2 object list.
 #'
 #' \code{add_bc_change_ratio()} adds base call change ratio (e.g.: editing frequency) 
 #' for each replicate and an average over all replictes for gDNA vs. cDNA comparisons
@@ -154,17 +155,19 @@ add_bc_change <- function(jacusa,
 #' boxplot(tapply(jacusa$bc_change_ratio, jacusa$bc_change, c))
 #' 
 #' @export 
-add_bc_change_ratio <- function(jacusa, 
-                                bc1 = jacusa$bc1, 
-                                bc2 = jacusa$bc2, bc_matrix2 = jacusa$bc_matrix2, 
+add_bc_change_ratio <- function(jacusa2, 
+                                ref,
+                                bc = jacusa2$bc, 
+                                bc_matrix = jacusa2$bc_matrix, 
                                 aa = FALSE) {
   # auto add missing fields
+  # TODO
   if (aa) {
-    jacusa <- add_bc_change(jacusa, aa = TRUE)
-    bc1 = jacusa$bc1
-    bc2 = jacusa$bc2
-    bc_matrix2 = jacusa$bc_matrix2
+    jacusa2 <- add_bc_change(jacusa2, aa = TRUE)
+    bc <- jacusa$bc
+    bc_matrix <- jacusa$bc_matrix
   }
-  jacusa <- c(jacusa, get_bc_change_ratio(bc1, bc2, bc_matrix2))
-  jacusa
+  # TODO
+  jacusa2 <- c(jacusa2, get_bc_change_ratio(bc, bc_matrix))
+  jacusa2
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Auxiliary R package for assessment of JACUSA1.x and JACUSA2.x results
 
 Under construction! 
+
+## Install
+
