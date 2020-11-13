9a2f76ec2200a479736eff65114db5723b3fa84d
qwang-big <qwang.big@gmail.com>
2020-11-13 13:19:06 +0100

    Create indel-ratio.R

---
+++ R/indel-ratio.R
@@ -0,0 +1,16 @@
+#' Extracted indel counts from the JACUSA2 info.
+#' 
+#' @param s character vector of the JACUSA2 info.
+#' @param f character of one of the following fields: "del1", "del2", "ins1", "ins2".
+#' @return data.frame of the extracted info.
+#' @export
+extract_info <- function(s,f){ 
+f = match.arg(f, c("del1", "del2", "ins1", "ins2"))
+d = str_extract(s, paste0(f,'\\d+=[\\d\\,]+'))
+d[!is.na(d)] = .eend(d[!is.na(d)], 7)
+d[is.na(d)]  = '0,0'
+.sp_mat(d)
+}
+
+.eend <- function(s,i) substr(s,i,nchar(s))
+.sp_mat <- function(d)read.table(textConnection(d), sep=',')
