57682e837dedd4934b2ae22d8eca398b5bfd569e
qwang-big <qwang.big@gmail.com>
2020-11-16 16:38:12 +0100

    add sub_counts

--- R/sub-ratio.R
+++ R/sub-ratio.R
@@ -66,3 +66,67 @@ sub_ratio <- function(ref, bases, bc = NULL) {
   
   sub_ratio
 }
+#' Calculates base substitution counts 
+#' 
+#' Calculates base substitution counts for \code{ref} and base counts stored
+#' in nx4 matrix \code{bases}.
+#' \code{ref} must have only one reference base. A->G is okay, but AG->G is NOT allowed!
+#' If \code{bc} is not provided, count the substitutions of the largest non-reference base
+#'
+#' @param ref vector of reference bases.
+#' @param bases matrix of observed base call counts.
+#' @param bc vector strings that represent observed base calls. Default: "bases".
+#' @return vector of base callsubstitution counts
+#' @examples
+#' ref <- c("A", "A", "T")
+#' # Only one non-ref. base
+#' bases <- matrix(c( 1,  0, 9, 0,
+#'                    5,  0, 5, 0,
+#'                    0, 10, 0, 0),
+#'                 ncol = 4, byrow = TRUE)
+#' # show base substiution counts for observed base calls
+#' sub_counts(ref, bases)
+#' 
+#' # > one non-ref. base
+#' bases <- matrix(c( 1,  2, 9, 1,
+#'                    5,  3, 5, 1,
+#'                    2, 10, 2, 1),
+#'                 ncol = 4, byrow = TRUE)
+#' # we are only interesected in A->G, T->C
+#' bc <- c("G", "G", "C")
+#' # show base substiution counts for A->G, T->C
+#' sub_counts(ref, bases, bc)
+#' @export
+sub_counts <- function(ref, bases, bc = NULL) {
+  colnames(bases) <- .BASES
+  
+  bc <- bc
+  if (is.null(bc)) {
+    d <- t(apply(bases, 1, sort.int, decreasing=TRUE, method = "quick"))
+    bc <- apply(bases==d[,2], 1, function(m) { 
+      return(paste0(.BASES[m][1], collapse = "")) 
+    })
+  }
+  
+  # calculates the variant base between ref and bc
+  variant_bc <- mapply(function(r, o) {
+    # remove reference base - only the variant base should remain
+    v <- gsub(r, "", o)
+    if (nchar(v) == 0) {
+      v <- r
+    }
+    if (nchar(v) >= 2) {
+      stop("More than 2 alleles not supported! ref: ", r, " observed BCs; ", o)
+    }
+    
+    return(v)
+  }, ref, bc, USE.NAMES = FALSE)
+  
+  # create variable to index 
+  # rows(1:length(variant_bc) and 
+  # cols(match(variant_bc, BASES))
+  # simultaneously in a matrix
+  i <- cbind(1:length(variant_bc), match(variant_bc, .BASES))
+  j <- cbind(1:length(ref), match(ref, .BASES))
+  data.frame(mis=bases[i], ref=bases[j])
+}
