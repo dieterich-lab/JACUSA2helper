be164801dcd97cb56f4a609fbe5c77a17044cd51
Michael Piechotta <7385779+piechottam@users.noreply.github.com>
2022-10-21 14:37:13 +0200

    Fixes typos. Added warning to extend. Added get_motif function and is_DRACH function.

---
+++ R/nmf.R
@@ -0,0 +1,20 @@
+#' Learn pattern from JACUSA2 output
+#' 
+#' @param 
+#' @param mods TODO 
+#' @return TODO
+#' 
+learn_nmf <- function(mods) {
+  # TODO
+}
+
+#' Predict modifications in JACUSA2 output
+#' 
+#' @param nmf TODO
+#'
+predict_mods <- function(nmf = NULL) {
+  if (is.null(nmf)) {
+    # TODO get learned NMF
+  }
+  # TODO
+}
---
+++ R/score.R
@@ -0,0 +1,7 @@
+#' @param gr TODO
+#' @param col TODO
+#' @param offset  
+context_score <- function(gr, col, offset) {
+  # TODO
+}
+
